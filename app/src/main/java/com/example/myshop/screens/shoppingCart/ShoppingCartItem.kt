3f7167f3041ed9a7316c81df549782e0c3980651
Staffan Pedersen <staffanbp@hotmail.com>
2023-12-17 21:44:10 +0100

    shoppingcart, design logic done

--- app/src/main/java/com/example/myshop/screens/shoppingCart/ShoppingCartItem.kt
+++ app/src/main/java/com/example/myshop/screens/shoppingCart/ShoppingCartItem.kt
@@ -2,102 +2,80 @@ package com.example.myshop.screens.shoppingCart
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.myshop.data.CartProducts
 
 
 @Composable
-fun ShoppingCartItem  (
+fun ShoppingCartItem(
     cartProduct: CartProducts,
     onRemoveClick: () -> Unit = {}
 ) {
-
-  //  val cartProducts = viewModel.cartProducts.collectAsState().value
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-
-        Spacer(modifier = Modifier.height(2.dp))
-
-            Row(
+        AsyncImage(
+            modifier = Modifier
+                .size(100.dp, 100.dp)
+                .background(color = Color.Gray),
+            model = cartProduct.image,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            contentDescription = "Image of ${cartProduct.title}"
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.7f)
+                .padding(start = 8.dp)
+        ) {
+            Text(
+                maxLines = 1,
+                text = cartProduct.title,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = cartProduct.category)
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(text = "In cart: ${cartProduct.quantity}")
+            Button(
+                onClick = onRemoveClick,
                 modifier = Modifier
-                    .padding(6.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-                //   .clickable {
-                //      onClick() }
+                    .padding(5.dp)
+                    .height(50.dp)
+                    .width(150.dp)
             ) {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(6.dp))
-                        .fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.secondary)
-                        .padding(6.dp)
-                        .height(100.dp)
-                ) {
-                    AsyncImage(
-                        modifier = Modifier
-                            .size(108.dp, 108.dp)
-                            .background(color = Color.Gray),
-                        model = cartProduct.image,
-                        alignment = Alignment.Center,
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "Image of ${cartProduct.title}"
-                    )
-                    Column(
-                        modifier = Modifier
-                            //.fillMaxHeight()
-                            .padding(start = 90.dp)
-                            .absoluteOffset(30.dp, 0.dp)
-                    ) {
-                        Text(
-                            text = cartProduct.title,
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = cartProduct.category)
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = "Price: $${cartProduct.price}")
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                    Text(text = "In cart: $${cartProduct.quantity}")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Button(onClick = onRemoveClick) {
-                        Text("Remove")
-                    }
-                }
+                Text(
+                    text = "X",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontSize = 15.sp)
+                )
             }
         }
     }
-
-
-//                Button(
-//                    onClick = {
-//                        viewModel.removeProductFromCart(cartProduct.id)
-//                    }
-//                ) {
-//                    Text(text = "Remove")
-//                }
-
+}
--- app/src/main/java/com/example/myshop/screens/shoppingCart/ShoppingCartListScreen.kt
+++ app/src/main/java/com/example/myshop/screens/shoppingCart/ShoppingCartListScreen.kt
@@ -1,14 +1,16 @@
 package com.example.myshop.screens.shoppingCart
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -19,14 +21,12 @@ fun ShoppinCartListScreen(
     viewModel: ShoppingCartViewModel,
     onBackButtonClick: () -> Unit = {},
     navController: NavController,
-    onProductClick: (productId: Int) -> Unit = {},
-
+    onProductClick: (productId: Int) -> Unit = {}
 ) {
     val cartProducts = viewModel.cartProducts.collectAsState().value
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize()
     ) {
         TopBar(
             title = "Shopping Cart",
@@ -36,15 +36,25 @@ fun ShoppinCartListScreen(
             navController = navController
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        LazyColumn {  // Use LazyColumn instead of Column
+        LazyColumn(
+            modifier = Modifier.weight(1f)  // This will make the LazyColumn take up all available space
+        ) {
             items(cartProducts) { cartProduct ->
                 ShoppingCartItem(
                     cartProduct = cartProduct,
                     onRemoveClick = { viewModel.removeProductFromCart(cartProduct.id) }
                 )
             }
+        }
+
+        Button(
+            onClick = { /* Handle place order click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(10.dp)
+        ) {
+            Text("Place order")
         }
     }
 }
